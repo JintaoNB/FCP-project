c2320d166a6cb35a15eea05293b8e8f9879797a3
JintaoNB <on23062@bristol.ac.uk>
2024-03-29 18:03:51 +0000

    first edition

--- task2.py
+++ task2.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-# 初始化参数
 grid_length = 100
 T = 0.5  # threshold
 beta = 0.5  # coupling
-steps = 100
+timestep = 100
 
 def initialize_opinions(size):
-    return np.random.random(size)
+    opinions = np.random.random(size)
+    return opinions
 
 
-def change_opinions(opinions, T, beta, steps):
-    for i in range(steps):
+def change_opinions(opinions, T, beta, timestep):
+    opinions_list = [opinions.copy()]
+    for a in range(timestep):
         i = np.random.randint(grid_length)
         if i == 0:
             j = i + 1
@@ -24,16 +25,15 @@ def change_opinions(opinions, T, beta, steps):
         if abs(opinions[i] - opinions[j]) < T:
             opinions[i] += beta * (opinions[j] - opinions[i])
             opinions[j] += beta * (opinions[i] - opinions[j])
+        opinions_list.append(opinions.copy())
+    return opinions_list
 
-    return opinions
+
+opinions_list = change_opinions(initialize_opinions(grid_length), T, beta, timestep)
 
 
-final_opinions = change_opinions(opinions, T, beta, steps)
-
-plt.figure(figsize=(10, 6))
-plt.plot(final_opinions, 'o')
-plt.title('Final Opinion Distribution')
-plt.xlabel('Individual')
-plt.ylabel('Opinion')
+plt.plot(opinions_list)
+plt.xlabel('timestep')
+plt.ylabel('opinion')
 plt.grid(True)
 plt.show()
